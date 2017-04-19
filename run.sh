ad4ca01a9c1a270e04fafcd2dbd7d58294b645c3
ideaworld <gbwlovehp@gmail.com>
2017-04-19 22:10:18 +0800

    update test cases

--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ echo "mongodb started, loading data..."
 sleep 1;
 cd /root/Conformance_Backend
 python load_test_type.py
-mongoimport -d fhirtest -c TestData --file testcases --type json
-
+mongoimport -d fhirtest -c FHIR_Analyzer --file Analyzer.json --type json
+mongoimport -d fhirtest -c DataType --file DataType.json --type json
+mongoimport -d fhirtest -c TestData --file TestData.json --type json
 echo "data loaded, starting server..."
 export C_FORCE_ROOT=true
 python listener/fhir_flask.py &
